290bd90c29c4a3bfd6c40e65bda090305d2cfa13
Eleanor Ruth <ellie.ruth@me.com>
2022-11-29 13:42:02 -0700

    create idea box

---
+++ src/components/App/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import './App.css'
+import Ideas from '../Ideas/Ideas'
+import Form from '../Form/Form'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      ideas: []
+    }
+  }
+
+  addIdea = (newIdea) => {
+    this.setState({ ideas: [...this.state.ideas, newIdea] })
+  }
+
+  deleteIdea = (id) => {
+    console.log(id)
+    const filteredIdeas = this.state.ideas.filter(idea => idea.id != id)
+
+    this.setState({ ideas: filteredIdeas})
+  }
+
+  render() {
+    return (
+      <main className='App'>
+        <h1>IdeaBox</h1>
+        <Form addIdea={this.addIdea} />
+        {!this.state.ideas.length && <h2>No ideas yet -- add some!</h2>}
+        <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea} />
+      </main>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Ideas/Ideas.js
@@ -0,0 +1,24 @@
+import React from "react"
+import './Ideas.css'
+import Card from '../Card/Card'
+
+const Ideas = ({ideas, deleteIdea}) => {
+  const ideaCards = ideas.map(idea => {
+    return (
+      <Card
+       title={idea.title}
+       description={idea.description}
+       id={idea.id}
+       key={idea.id}
+       deleteIdea={deleteIdea}/>
+
+    )
+  })
+  return (
+    <div className='ideas-container'>
+      {ideaCards}
+    </div>
+  )
+}
+
+export default Ideas
