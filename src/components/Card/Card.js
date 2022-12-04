e8bcf58cabbdd0be112a6cdb14590d1c9090e7a1
Eleanor Ruth <ellie.ruth@me.com>
2022-12-03 17:11:19 -0700

    Add Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './Card.css'
+
+const Card = ({title, description, id, deleteIdea}) => {
+  return (
+    <div className="card">
+      <p>title: {title} </p>
+      <p>idea: {description}</p>
+      <button onClick={() => deleteIdea(id)}>Delete</button>
+    </div>
+  )
+}
+
+export default Card
