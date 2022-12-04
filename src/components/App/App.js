c1b72470a224cc40a301919f6bf1583cf55e02c4
Eleanor Ruth <ellie.ruth@me.com>
2022-12-03 17:10:54 -0700

    Build App component

---
+++ src/components/App/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react"
+import ideasArray from "../../data"
+import Ideas from "../Ideas/Ideas"
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      ideas: ideasArray
+    }
+  }
+
+  deleteIdea = (id) => {
+    const ideasArra = this.state.ideas.filter((idea) => {
+      return idea.id != id 
+    })
+    this.setState({ideas: ideasArra})
+  }
+  render() {
+    return (
+      <main>
+        <h1>IdeaBox</h1>
+        <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea}/>
+      </main>
+    )
+  }
+}
+
+export default App
