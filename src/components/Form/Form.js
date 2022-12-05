dd7fd198ba7f76bb35180dec7642fe506c48e174
Eleanor Ruth <ellie.ruth@me.com>
2022-12-05 15:11:04 -0700

    Add render method to Form function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,11 +2,31 @@ import React, { Component } from "react";
 import '.Form/css'
 
 class Form extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
-
     }
+  }
+  render() {
+    return (
+    <form>
+      <input
+        placeholder="title"
+        value={this.state.title}
+        name="title"
+        type="text"
+        onChange={this.handleChange}
+      />
+      <input
+        placeholder="description"
+        value={this.state.description}
+        name="description"
+        type="text"
+        onChange={this.handleChange}
+      />
+      <button>Submit</button>
+    </form>
+    )
   }
 }
 
