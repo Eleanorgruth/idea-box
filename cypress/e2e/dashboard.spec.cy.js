be6071267830568c5c2d7d8fa72a058bca450361
Eleanor Ruth <ellie.ruth@me.com>
2022-12-05 19:14:04 -0700

    Write test for pageload

--- cypress/e2e/dashboard.spec.cy.js
+++ cypress/e2e/dashboard.spec.cy.js
@@ -1,5 +1,14 @@
+beforeEach(() => {
+  cy.visit('http://localhost:3000');
+});
 describe('Dashboard view page', () => {
   it('Should confirm that true is equal to true', () => {
     expect(true).to.equal(true)
-  });
+  })
+  it('Should be able to visit the page and render the correct elements', () => {
+    cy.visit('http://localhost:3000')
+      .get('h1').contains('IdeaBox')
+      .get('form')
+      .get('.ideas')
+  })
 });
