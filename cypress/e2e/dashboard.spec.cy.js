fa7f3e636bc493962503ba847a6ae364cb34006a
Eleanor Ruth <ellie.ruth@me.com>
2022-12-05 19:22:51 -0700

    Write test for for,

--- cypress/e2e/dashboard.spec.cy.js
+++ cypress/e2e/dashboard.spec.cy.js
@@ -1,14 +1,21 @@
-beforeEach(() => {
-  cy.visit('http://localhost:3000');
-});
 describe('Dashboard view page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
   it('Should confirm that true is equal to true', () => {
     expect(true).to.equal(true)
   })
   it('Should be able to visit the page and render the correct elements', () => {
-    cy.visit('http://localhost:3000')
-      .get('h1').contains('IdeaBox')
+    cy.get('h1').contains('IdeaBox')
       .get('form')
       .get('.ideas')
   })
-});
+  it('Should have an input box to submit new ideas', () => {
+    cy.get('[placeholder="title"]')
+      .type('test')
+      .should('have.value', 'test')
+    cy.get('[placeholder="description"]')
+      .type('test2')
+      .should('have.value', 'test2')
+  })
+})
