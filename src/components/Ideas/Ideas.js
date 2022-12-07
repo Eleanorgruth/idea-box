efe1a66576cb198c736c3446b7951788525465dc
Eleanor Ruth <ellie.ruth@me.com>
2022-12-07 07:33:41 -0700

    Create Ideas component

--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -1,10 +1,22 @@
 import React from "react";
 import './Ideas.css'
+import Card from '../Card/Card'
 
-const Ideas = () => {
+const Ideas = ({ideas}) => {
+  const ideasArray = ideas.map((idea)=>{
+    return (
+      <Card
+        item={idea.name}
+        amount={idea.amount}
+        id={idea.id}
+        key={idea.id}
+        />
+    )
+  })
 return (
   <div>
-    <h2>list items</h2>
+    <h2>list items</h2> 
+    {ideasArray}
   </div>
 )
 }
