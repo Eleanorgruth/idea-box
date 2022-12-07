0e7bb88779d7fbb18a5ac113ce6ebf7c8ae4dcca
Eleanor Ruth <ellie.ruth@me.com>
2022-12-07 08:23:27 -0700

    Pass deleteItem in as a prop

--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -2,10 +2,11 @@ import React from "react";
 import './Ideas.css'
 import Card from '../Card/Card'
 
-const Ideas = ({ideas}) => {
+const Ideas = ({ideas, deleteItem}) => {
   const ideasArray = ideas.map((idea)=>{
     return (
       <Card
+        deleteItem={deleteItem}
         item={idea.name}
         amount={idea.amount}
         id={idea.id}
