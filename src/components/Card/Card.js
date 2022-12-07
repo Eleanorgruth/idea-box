d738a5b376265917dd658a907c0ed0dab4c84068
Eleanor Ruth <ellie.ruth@me.com>
2022-12-07 08:22:17 -0700

    Add delete button function

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from "react"
 import './Card.css'
 
-const Card = ({item, amount}) => {
+const Card = ({item, amount, deleteItem, id}) => {
   return (
     <div>
       <p>Item: {item}</p>
       <p>Amount: {amount}</p>
+      <button onClick={()=>deleteItem(id)}>🗑</button>
     </div>
   )
 }
